b36e1cd923027940ec1704773303d9140a8991af
honey-barbara <susie@Johns-MacBook-Pro-2.local>
2015-08-13 11:32:11 -0500

    Groundwork for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,7 +8,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
