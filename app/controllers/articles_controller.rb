c1c0514a8d3508a13bf1296be4378fdd32af703e
honey-barbara <susie@Johns-MacBook-Pro-2.local>
2015-08-13 12:23:40 -0500

    Completed article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,17 @@
 class ArticlesController < ApplicationController
   def new
   end
-    
+  def create
+    allowed_params = params.require(:article).permit(:title,:text)
+    @article = Article.new(allowed_params)
+    @article.save
+    redirect_to @article
+   end
+   def show
+     @article = Article.find(params[:id])
+     
+
+
+   end
+
 end
